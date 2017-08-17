e5cb8dcf8fa2ad68498b6346c9700f23b7bb437f
TENCENT\ruichu <ruichu@tencent.com>
2017-08-17 21:52:51 +0800

    计算逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,27 @@
 //index.js
 //获取应用实例
 var app = getApp();
+var input_X;
+var condition_value1;
+var condition_text_value1;
+var condition_value2;
+var condition_text_value2;
+var condition_value3;
+var condition_text_value3;
+var condition_value3_Y;
+var condition_text_valueY;
+var condition_value3_A;
+var condition_text_valueA;
+var condition_value3_B;
+var condition_text_valueB;
 var result_value;
+
 Page({
   data: {
-    input_price_value:"",
-    array_one: ['请选择','Android', 'IOS', 'ReactNativ', 'WeChat', 'Web'],
-    array_two: ['请选择','爱我', '亲我', '吻我'],
-    array_three: ['请选择','爱你', '亲你', '吻你'],
+    input_price:"",
+    array_one: ['请选择', '增值税专用发票', '增值税普通发票'],
+    array_two: ['请选择', '金融服务（营业用）', '业务宣传费-赠送', '购进 / 租入有形动产用于出租 / 转租','购进/租入有形动产用于出租/转租'],
+    array_three: ['请选择', '3%', '6%','11%', '17%'],
     array_four: ['请选择','么么哒', '么么', '你最美'],
     index_one:0,
     index_two: 0,
@@ -33,8 +47,10 @@ Page({
   },
   
   input_price:function(e){
+    input_X = e.detail.value;
+    console.log(input_X);
     this.setData({
-      input_price_value: e.detail.value
+      input_price: e.detail.value
     });
   },
 
@@ -43,24 +59,25 @@ Page({
     this.setData({
       index_one: e.detail.value
     });
-    switch (e.detail.value) {
-      case 0:
-        condition_value1 = e.detail.value;
-        console.log(condition_value1);
-        break;
+    condition_value1=e.detail.value;
+    console.log(condition_value1);
+    if (condition_value1==1){
+      condition_text_value1 = "增值税专用发票"
+      console.log(condition_text_value1);
+    }else{
+      condition_text_value2 = "增值税普通发票"
+      console.log(condition_text_value2);
+    }
+    switch (condition_value1) {
       case 1:
-        condition_value1 = e.detail.value;
-        console.log(condition_value1);
+        condition_text_value1 ="增值税专用发票"
+        console.log(condition_text_value1);
         break;
       case 2:
-        condition_value1 = e.detail.value;
-        console.log(condition_value1);
+        condition_text_value2 = "增值税普通发票"
+        console.log(condition_text_value2);
         break;
-      case 3:
-        condition_value1 = e.detail.value;
-        console.log(condition_value1);
-        break;
-    }
+    };
   },
   /**
 * 监听普通picker选择器
@@ -70,21 +87,38 @@ Page({
     this.setData({
       index_two: e.detail.value
     });
+    condition_value2 = e.detail.value;
+    console.log(condition_value2);
+    if (e.detail.value == 1 && e.detail.value == 2) {
+      this.setData({
+        array_three: ['请选择', '3%','6%','11%', '17%'],
+      })
+    };
+    if(e.detail.value==3){
+      this.setData({
+        array_three: ['请选择', '3%','11%', '17%'],
+      })    
+    };
+    if (e.detail.value == 4) {
+      this.setData({
+        array_three: ['请选择', '5%', '11%'],
+      })
+    };
     switch (e.detail.value) {
       case 0:
-        condition_value2 = e.detail.value;
+        
         console.log(condition_value2);
         break;
       case 1:
-        condition_value2 = e.detail.value;
+        
         console.log(condition_value3);
         break;
       case 2:
-        condition_value2 = e.detail.value;
+        
         console.log(condition_value2);
         break;
       case 3:
-        condition_value2 = e.detail.value;
+        
         console.log(condition_value2);
         break;
     }
@@ -97,21 +131,23 @@ Page({
     this.setData({
       index_three: e.detail.value
     });
+    condition_value3 = e.detail.value;
+    console.log(condition_value3);
     switch (e.detail.value) {
       case 0:
-        condition_value3 = e.detail.value;
+        
         console.log(condition_value3);
         break;
       case 1:
-        condition_value3 = e.detail.value;
+        
         console.log(condition_value3);
         break;
       case 2:
-        condition_value3 = e.detail.value;
+        
         console.log(condition_value3);
         break;
       case 3:
-        condition_value3 = e.detail.value;
+        
         console.log(condition_value3);
         break;
     }
@@ -147,7 +183,23 @@ Page({
   }, 
 
   button_compute:function(e){
-    result_value=520;
+    
+    if(condition_value1==1){
+      console.log(condition_value1);
+       if(condition_value2==1){
+         console.log(condition_value2);
+         result_value=input_X+"  "+"fapiao ";
+       }
+       else if(condition_value2==2){
+         if(condition_value3==1){
+           result_value=input_X/1.03*1.17
+         }else{
+           result_value=input_X;
+         }
+       }
+    }
+
+    // result_value=520;
     this.setData({
       result_text: result_value
     })
@@ -163,7 +215,7 @@ Page({
     
     result_value = "";
     this.setData({
-      input_price_value: "",
+      input_price: "",
       index_one: 0,
       index_two: 0,
       index_three: 0,
